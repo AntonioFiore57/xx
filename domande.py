4bbda09775971a5bbc467fe26fa117be2147c0ad
tony <antonio.fiore57@virgilio.it>
2024-03-14 11:04:37 +0100

    dizionario domande OK lista players OK

--- domande.py
+++ domande.py
@@ -39,6 +39,7 @@ class Domanda:
         self.lst_perm = list(self.perm)
         self._NUMERO_COMB = len(self.lst_perm)
         self._index_permutazione_risposte = randint(0, self._NUMERO_COMB - 1)
+        self.rispostaCorretta = self.lst_perm[self._index_permutazione_risposte].index(0)
     def dispDomanda(self):
         '''
         schema visualizzazione
@@ -59,11 +60,63 @@ class Domanda:
         for k in self.lst_perm[n]:
             uscita = uscita + str(i) +'. ' + self.risposte[k] +'\n'
             i += 1
-        uscita = uscita + "\n\n"
-
         return uscita
+    def checkRisposta(self, n):
+        if n not in (1,2,3,4):
+            raise ValueError("Valore non consentito")
+        if self.rispostaCorretta == n - 1:
+            # risposta esatta
+            return True
+        else:
+            return False
 
-
+"""
 d1 = Domanda("Lingua ufficiale del Brasile?", 0, ['Portoghese', 'Spagnolo', 'Urdu', 'Klingon'])
 
 print(d1.dispDomanda())
+risposta = int(input("Inserisci la risposta: "))
+if d1.checkRisposta(risposta):
+    print("Risposta corretta!")
+else:
+    print(f"Risposta sbagliata! La risposta corretta era: {d1.rispostaCorretta +1}")
+"""
+
+# Dal contenuto del file domande.txt si ricava un dizionario in cui le chiavi sono il livello
+# di difficoltà delle domande e il valore una lista delle domande con quel grado di difficoltà.
+#
+#
+f = open("F:\\U3E\\TdP_Corno\\TdP2024\\Lab_01\\domande.txt", 'r', encoding='utf-8')
+righe = [y for y in [x.strip() for x in f.readlines()] if y!='' ]
+f.close()
+dict_domande = {}
+
+
+for i in range(len(righe)//6):
+    d = Domanda(righe[i*6], righe[i*6+1], [righe[i*6+2], righe[i*6+3], righe[i*6+4], righe[i*6+5]   ])
+    if int(righe[i*6+1]) not in dict_domande:
+        dict_domande[int(righe[i*6+1])] = list()
+        dict_domande[int(righe[i * 6 + 1])].append(d)
+    else:
+        dict_domande[int(righe[i * 6 + 1])].append(d)
+
+
+# I punti dei giocatori vengono gestiti con una lista di tuple
+# [ (nikname1, punti), (nikname2, punti) .... ]
+players = list()
+f = open("F:\\U3E\\TdP_Corno\\TdP2024\\Lab_01\\punti.txt", 'r', encoding='utf-8')
+righe = [x.strip() for x in f.readlines()]
+f.close()
+
+for riga in righe:
+    nikname = riga.split()
+    players.append( (nikname[0], int(nikname[1])  ) )
+
+# non ci preoccupiamo se un nikname è già presente
+
+
+nikname = input("Inserisci il tuo nikname: ")
+
+gioco = True
+punti = 0
+livello = 0
+while gioco:
